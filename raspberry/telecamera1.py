80282307d6a91af6c7db6694388cd2895ef5b08b
HappyJerry2445 <114814548+HappyJerry2445@users.noreply.github.com>
2023-04-18 15:44:38 +0200

    Update telecamera1.py

--- raspberry/telecamera1.py
+++ raspberry/telecamera1.py
@@ -3,10 +3,15 @@ import numpy as np
 import pyzbar.pyzbar as pyzbar
 import threading
 import time
+import socket
+import motore
 
 class CameraScanner:
 
     def __init__(self, camera_id):
+        self.reciverHost= '172.16.12.14'
+        self.reciverPort= 12345
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.primaScansione = True
         self.funzione = False
         self.ultimo_tempo_scansione = 0
@@ -39,7 +44,7 @@ class CameraScanner:
         for obj in decodedObjects:
             res= obj.data.decode("utf-8")
             print("QR Code trovato! Contenuto: ", res)
-            print(f"""QR = {obj.data} primaScansione = {self.primaScansione} funzione={'Ingresso' if self.primaScansione else 'Uscita'}""")
+            print(f"""QR = {res} primaScansione = {self.primaScansione} funzione={'Ingresso' if self.primaScansione else 'Uscita'}""")
             if res == "INGRESSO" and self.primaScansione:
                 self.funzione= True
                 self.primaScansione=False
@@ -47,8 +52,15 @@ class CameraScanner:
                 self.funzione= False
                 self.primaScansione=False
             else:
-                pass
-                #SEND SOCKET
+                motore.start()
+                try:
+                    print("SOCKET")
+                    self.sock.connect((self.reciverHost,self.reciverPort))
+                    self.sock.send(res.encode())
+                    self.sock.close()
+                except Exception as e:
+                    print("Socket rotta")
+                    print(e)
             time.sleep(10)
 
 if __name__ == "__main__":
