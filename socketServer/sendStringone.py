35af11a406baaa4a219b9f9cff13067c5005b943
Antopiogar <103059144+Antopiogar@users.noreply.github.com>
2023-05-04 12:46:06 +0200

    Add files via upload

--- socketServer/sendStringone.py
+++ socketServer/sendStringone.py
@@ -6,7 +6,7 @@ PORT = 12345  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.send("!1!sUd1X!39dfa55283318d31afe5a3ff4a0e3253e2045e43!2023/4/2 16:27:7!UjQx3".encode())
+    s.send("afjaksfh!i!1!sUd1X!39dfa55283318d31afe5a3ff4a0e3253e2045e43!2023/04/02 16:27:07!UjQx3".encode())
     data = s.recv(1024)
 
 print(f"Received {data!r}")
