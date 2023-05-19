8759744a0f8c02adeef3734ee1fdcb1bba5ff41d
Antopiogar <103059144+Antopiogar@users.noreply.github.com>
2023-05-19 10:35:17 +0200

    Add files via upload

---
+++ BackEnd/Control/gkGenerator.py
@@ -0,0 +1,15 @@
+import hashlib
+import sys
+import secrets
+sys.path.append('./BackEnd/Model/')
+import accountDAO as db_A
+
+def gkGenerator():
+    #prende la lista di gk di ogni utente
+    listGK = db_A.getAllAccountGK()
+    #crea una gk univoca di 4 caratteri comprendente lettere e numeri in formato esadecimale
+    gk = hashlib.sha1(secrets.token_hex(2).encode()).hexdigest()
+    
+
+    if gk not in listGK:
+        return gk.strip()
